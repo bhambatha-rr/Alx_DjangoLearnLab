d503b6c9ce53ae75b05c4d9f39c00238c46cea1b
bhambatha-rr <ntokozomahwayi067@gmail.com>
2025-07-24 19:21:54 +0200

    Implementing Custom Permissions in Djangos

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,24 +1,16 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.decorators import login_required, permission_required, user_passes_test
+from django.contrib.auth.decorators import login_required, permission_required  # ADD THIS IMPORT
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 from .models import Book, Library, UserProfile
 from .forms import BookForm
-
-def check_admin(user):
-    return user.userprofile.role == 'ADMIN'
-
-def check_librarian(user):
-    return user.userprofile.role == 'LIBRARIAN'
-
-def check_member(user):
-    return user.userprofile.role == 'MEMBER'
 
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-@permission_required('relationship_app.can_add_book')
+@permission_required('relationship_app.can_add_book', raise_exception=True)  # ADD DECORATOR
 def add_book(request):
     if request.method == 'POST':
         form = BookForm(request.POST)
@@ -31,7 +23,7 @@ def add_book(request):
         form = BookForm()
     return render(request, 'relationship_app/book_form.html', {'form': form})
 
-@permission_required('relationship_app.can_change_book')
+@permission_required('relationship_app.can_change_book', raise_exception=True)  # ADD DECORATOR
 def edit_book(request, pk):
     book = get_object_or_404(Book, pk=pk)
     if request.method == 'POST':
@@ -45,7 +37,7 @@ def edit_book(request, pk):
         form = BookForm(instance=book)
     return render(request, 'relationship_app/book_form.html', {'form': form})
 
-@permission_required('relationship_app.can_delete_book')
+@permission_required('relationship_app.can_delete_book', raise_exception=True)  # ADD DECORATOR
 def delete_book(request, pk):
     book = get_object_or_404(Book, pk=pk)
     if request.method == 'POST':
@@ -59,25 +51,10 @@ def register(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('member_view')
+            return redirect('list_books')
     else:
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
-
-@login_required
-@user_passes_test(check_admin)
-def admin_view(request):
-    return render(request, 'relationship_app/admin_view.html')
-
-@login_required
-@user_passes_test(check_librarian)
-def librarian_view(request):
-    return render(request, 'relationship_app/librarian_view.html')
-
-@login_required
-@user_passes_test(check_member)
-def member_view(request):
-    return render(request, 'relationship_app/member_view.html')
 
 class CustomLoginView(LoginView):
     template_name = 'relationship_app/login.html'
