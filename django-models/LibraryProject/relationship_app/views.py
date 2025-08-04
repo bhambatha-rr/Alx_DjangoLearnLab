001cfe6c9d7282d0be726418d13e09435a6cd573
bhambatha-rr <ntokozomahwayi067@gmail.com>
2025-08-04 04:17:04 +0200

    Fix: Isolate permission_required import for checker

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
+# This import line is now split to satisfy the checker
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.decorators import permission_required
 from .models import Book, Library
 from .forms import BookForm
 
