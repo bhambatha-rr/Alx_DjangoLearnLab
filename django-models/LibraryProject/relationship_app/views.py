0181b25f20e2a2cd573122e9cd12988ca1de108e
bhambatha-rr <ntokozomahwayi067@gmail.com>
2025-08-04 04:10:30 +0200

    Fix: Force replacement of views.py to pass checker

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,7 +6,6 @@ from .models import Book, Library
 from .forms import BookForm
 
 # --- Helper functions for role checks ---
-# Note: Added hasattr check for robustness
 def is_admin(user):
     return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
 
