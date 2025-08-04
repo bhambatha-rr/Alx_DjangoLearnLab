83e7b080aad41d04e8f47f385151d56b8d329eee
bhambatha-rr <ntokozomahwayi067@gmail.com>
2025-08-04 04:57:43 +0200

    Feat: Implement CustomUserManager and update CustomUser model

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,15 +1,11 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser, Book # Make sure to import Book if it's also registered here
+from .models import CustomUser, Book
 
-class CustomUserAdmin(UserAdmin):
-    fieldsets = UserAdmin.fieldsets + (
-        ('Custom Fields', {'fields': ('date_of_birth', 'profile_photo')}),
-    )
-    add_fieldsets = UserAdmin.add_fieldsets + (
-        ('Custom Fields', {'fields': ('date_of_birth', 'profile_photo')}),
-    )
-    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'date_of_birth')
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'date_of_birth')
+    search_fields = ('email', 'first_name', 'last_name')
+    list_filter = ('is_staff', 'is_active')
+    ordering = ('email',)
 
 # Register your models
 admin.site.register(CustomUser, CustomUserAdmin)
